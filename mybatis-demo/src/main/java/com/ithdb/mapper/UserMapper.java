01eb4dec1d069456bb34d55fbba2f691ff635d4b
hollis <herryhollis@163.com>
2024-01-14 21:12:31 +0800

    注解开发

--- mybatis-demo/src/main/java/com/ithdb/mapper/UserMapper.java
+++ mybatis-demo/src/main/java/com/ithdb/mapper/UserMapper.java
@@ -1,11 +1,16 @@
 package com.ithdb.mapper;
 
 import com.ithdb.pojo.User;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface UserMapper {
     List<User> selectAll();
 
+    @Select("select * from tb_user where id = #{id};")
     User selectById(int id);
+
+    User select(@Param("username") String username, @Param("password") String password);
 }
